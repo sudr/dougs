e46e524e17e4f5b6c1413af3b148544d3a955250
sudr <to_sud@yahoo.com>
2010-09-29 23:06:43 -0500

    hfooad:ch2 - initial skeleton for dougs dog door app

---
+++ src/main/java/info/sudr/hfooad/dougs/App.java
@@ -0,0 +1,13 @@
+package info.sudr.hfooad.dougs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
